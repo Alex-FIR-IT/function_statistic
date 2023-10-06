9cecd85542d8d4e73a677b0bc070d15058523531
zhandrov@firsov-vostro-3490 <osintfir@gmail.com>
2023-10-06 14:44:18 +0300

    add annotation delete commented code

--- additional.py
+++ additional.py
@@ -19,21 +19,15 @@ class Log:
         raise AttributeError(f"Класс {cls.__name__} не поддерживает создание экземпляров")
 
     @staticmethod
-    def dict_to_log(dct: dict, all_instances: bool = True, instance_class_name: str = "Statistic", sep = "; ", end = "\n"):
-        date_time = datetime.datetime.now()
-        start = f"[{date_time}] {instance_class_name} INFO:"
-        # instances = [f"\t[{key}: {value}]"
-        #              for func_info in dct.values()
-        #              for key, value in func_info.items()]
-
+    def dict_to_log(dct: dict, instance_class_name: str = "Statistic", sep: str = "; ", end: str = "\n") -> str:
         if len(dct) == 1:
             dct = {1: dct}
 
-        instances = ["\t" + "; ".join([f"{key}: {value}"
+        instances = ["\t" + f"{sep}".join([f"{key}: {value}"
                      for key, value in func_info.items()])
                      for func_info in dct.values()]
 
-
+        start = f"[{datetime.datetime.now()}] {instance_class_name} INFO:"
         instances.insert(0, start)
         output = f"{end}".join(instances)
 
